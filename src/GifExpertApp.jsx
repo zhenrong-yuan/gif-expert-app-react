1aa4b6268b293ef2ce52e2ee09095fd5012b9f96
Zhenrong Yuan <zhenrong.yuanlu@gmail.com>
2024-10-24 16:19:25 -0300

    index.js

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { AddCategory } from "./components/addCategory";
-import { GifGrid } from "./components/GifGrid";
+import { AddCategory, GifGrid } from "./components";
 
 
 export const GifExpertApp = () => {
