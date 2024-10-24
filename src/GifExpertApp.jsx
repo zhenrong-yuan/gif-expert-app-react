57a0031742114581642619c0db853b39a098e7cb
Zhenrong Yuan <zhenrong.yuanlu@gmail.com>
2024-10-23 21:06:41 -0300

    Create GifExpertApp.jsx

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,28 @@
-
+import { useState } from "react";
+import { AddCategory } from "./components/addCategory";
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Zoro', 'Luffy', 'Nami']);
+    
+    const onAddCategory = (newCategory) => {
+
+        if ( categories.includes(newCategory) ) return; 
+        setCategories([... categories, newCategory]);
+    }
+    
     return (
         <>
             <h1>GifExpertApp</h1>
+            <AddCategory 
+                //setCategories={ setCategories } 
+                onNewCategory={ onAddCategory }
+            />
+            <ol>
+                { categories.map( category => {
+                        return <li key={ category } > { category } </li>
+                    }) 
+                }
+            </ol>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { GifExpertApp } from './GifExpertApp'
+import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GifExpertApp />
   </StrictMode>,
 )
